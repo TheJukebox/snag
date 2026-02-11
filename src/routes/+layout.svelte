2f6ae88b8bb0e6a725fa508172a13ea5d31032ac
jukebox <5196313+TheJukebox@users.noreply.github.com>
2026-02-11 17:45:21 +1100

    Update title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import sausage from '$lib/assets/sausage.png';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head><title>snag</title><link rel="icon" href={sausage} /></svelte:head>
 {@render children()}
